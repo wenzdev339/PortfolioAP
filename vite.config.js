235e1a7e22e68164ade5f3d8ce742184eb25e2fe
wenzdev339 <109328287+wenzdev339@users.noreply.github.com>
2025-04-08 13:31:28 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import profilePhoto from './assets/profile-photo.png'
 
 // https://vite.dev/config/
 export default defineConfig({
